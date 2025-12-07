646d69eb22cd6c4815d4ca6c6193fc55c5f63c4c
Landmark New Homes LTD. <landmarknewhomesltd@gmail.com>
2025-12-07 06:26:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-363001551-87a23",
+  "appId": "1:32971444792:web:b7b93efab67b28842e8ccd",
+  "apiKey": "AIzaSyADX9r7Aoxa--ea8EoDxaM2Jqnkhlm11m0",
+  "authDomain": "studio-363001551-87a23.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "32971444792"
+};
